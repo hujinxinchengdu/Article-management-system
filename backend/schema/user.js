b476a1ba7cea35ee5a7adf36bc390da41aafeb36
Jinxin Hu <hujinxin0607@gmail.com>
2021-11-02 16:26:32 -0700

    merge from fwr's file because  some conflict problem. all fix

--- backend/schema/user.js
+++ backend/schema/user.js
@@ -15,10 +15,39 @@ const password = joi
     .pattern(/^[\S]{6,18}$/)
     .required()
 
+
+// 定义 id, nickname, emial 的验证规则
+const userId = joi.number().integer().min(1).required();
+const nickName = joi.string().required();
+const email = joi.string().email().required();
+
 exports.reg_login_schema = {
 
     body: {
         username,
         password,
+    },
+}
+
+// 验证规则对象 - 更新用户基本信息
+exports.update_userinfo_schema = {
+    body: {
+        userId,
+        nickName,
+        email,
+    },
+};
+
+// 验证规则对象 - 重置密码
+exports.update_password_schema = {
+    body: {
+        // 使用 password 这个规则，验证 req.body.oldPwd 的值
+        oldPwd: password,
+        // 使用 joi.not(joi.ref('oldPwd')).concat(password) 规则，验证 req.body.newPwd 的值
+        // 解读：
+        // 1. joi.ref('oldPwd') 表示 newPwd 的值必须和 oldPwd 的值保持一致
+        // 2. joi.not(joi.ref('oldPwd')) 表示 newPwd 的值不能等于 oldPwd 的值
+        // 3. .concat() 用于合并 joi.not(joi.ref('oldPwd')) 和 password 这两条验证规则
+        newPwd: joi.not(joi.ref('oldPwd')).concat(password),
     },
 }
