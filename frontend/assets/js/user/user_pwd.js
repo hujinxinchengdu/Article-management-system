cbc2975cf000508ed897a49bb6e4a0f3322502df
Wenrui-Fang <fangwenrui1997@outlook.com>
2021-11-07 16:05:13 -0800

    fix merge

--- frontend/assets/js/user/user_pwd.js
+++ frontend/assets/js/user/user_pwd.js
@@ -1,39 +1,4 @@
 $(function () {
-<<<<<<< HEAD
-    var form = layui.form;
-
-    form.verify({
-        pwd: [/^[\S]{6,12}$/, "The password must be 6-12 digits and no spaces"],
-        samePwd: function (value) {
-            if (value === $("[name=oldPwd]").val()) {
-                return "The old and new passwords cannot be the same!";
-            }
-        },
-        rePwd: function (value) {
-            if (value !== $("[name=newPwd]").val()) {
-                return "The two passwords are inconsistent！";
-            }
-        },
-    });
-
-    $(".layui-form").on("submit", function (e) {
-        e.preventDefault();
-        $.ajax({
-            method: "POST",
-            url: "/my/updatepwd",
-            data: $(this).serialize(),
-            success: function (res) {
-                if (res.status !== 0) {
-                    return layui.layer.msg("Failed to update password!");
-                }
-                layui.layer.msg("Succeeded to update password!");
-                // Reset form
-                $(".layui-form")[0].reset();
-            },
-        });
-    });
-});
-=======
   var form = layui.form;
 
   form.verify({
@@ -67,4 +32,3 @@ $(function () {
     });
   });
 });
->>>>>>> Wenrui-development
